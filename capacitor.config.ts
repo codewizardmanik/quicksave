3994cb62360899baeb29d418de2813c6e7260822
beigestack <m4.sh@tuta.io>
2026-06-01 10:11:48 +0530

    yes

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'xyz.vncl.quicksave',
+  appName: 'quicksave',
+  webDir: 'build'
+};
+
+export default config;
